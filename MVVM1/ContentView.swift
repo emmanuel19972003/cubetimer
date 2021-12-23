61cb9838c0a52a688b12667d8876d2a6d71bb171
emmanuel19972003 <emmanuel1997@gmail.com>
2021-12-23 18:42:30 -0500

    Initial Commit

---
+++ MVVM1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MVVM1
+//
+//  Created by Emmanuel Zambrano Quitian on 12/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MVVM1/MVVM1App.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM1App.swift
+//  MVVM1
+//
+//  Created by Emmanuel Zambrano Quitian on 12/23/21.
+//
+
+import SwiftUI
+
+@main
+struct MVVM1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
